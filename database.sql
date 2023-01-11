80511bcd0e443b9dfd245e267be0d03ca65d8cfa
NunoMatosCorreia <NUNO CORREIA>
2023-01-11 15:12:01 +0000

    update names in data base cars

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
-INSERT INTO cars (company_id, Name, Number_passengers, Number_doors, gear_box, Km, Price, available, image)
+INSERT INTO cars (company_id, name, number_passengers, number_doors, gear_box, Km, price, available, image)
 VALUES (1, "Ferrari - Enzo", 2, 3, "Manual", 1500, 1200.00, "True", "https://images.pexels.com/photos/337909/pexels-photo-337909.jpeg"),
 (1, "Audi - A4", 5, 5, "Manual", 25000, 140.00, "True", "https://images.pexels.com/photos/244206/pexels-photo-244206.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"),
 (1, "Range Rover - Evoque", 5, 3, "Automatic", 10000, 195.00, "True", "https://images.pexels.com/photos/116675/pexels-photo-116675.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"),
